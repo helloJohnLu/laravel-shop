8d3756bdaefc59376572aa8fdb51a16dd2676640
vagrant <vagrant@homestead>
2022-04-28 20:42:22 +0000

    登录与注册

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,14 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  {{-- CSRF Token --}}
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
-
-  <title>@yield('title', 'Laravel Shop') - Laravel 电商网站</title>
-
-  {{-- 样式 --}}
-  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  <title>@yield('title', 'Laravel Shop') - Laravel 电商教程</title>
+  <!-- 样式 -->
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
 <div id="app" class="{{ route_class() }}-page">
@@ -20,7 +18,7 @@
   </div>
   @include('layouts._footer')
 </div>
-{{-- JS --}}
+<!-- JS 脚本 -->
 <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
